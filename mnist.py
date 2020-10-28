5a8457d37ac07beaba9ef41a8cb58348a8cae274
Shishuai Wang <822074+snowolf@users.noreply.github.com>
2020-10-28 15:37:33 +0800

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -199,8 +199,8 @@ if __name__ == '__main__':
     # Container environment
     parser.add_argument('--hosts', type=list, default=json.loads(os.environ['SM_HOSTS']))
     parser.add_argument('--current-host', type=str, default=os.environ['SM_CURRENT_HOST'])
-    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
-    parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
+    parser.add_argument('--model_dir', type=str, default='/opt/ml/model')
+    parser.add_argument('--data_dir', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
     parser.add_argument('--num-gpus', type=int, default=os.environ['SM_NUM_GPUS'])
 
     args = parser.parse_args()
